ea152bfdef71ca3a81dfa5fa6431c042e39e902c
Melissa <melissarenaut@gmail.com>
2017-11-09 16:13:01 -0500

    updated readme and some comments

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# bamazon
-Amazon-like storefront using nodejs and mysql
+# Bamazon
+Amazon-like storefront written in JavaScript, with NodeJS, SQL and MySQL.
+
+
+
+## Amazon-like storefront console application 
+
+User interacts using the Command Line Interface.  Inventory is kept in a SQL database.  
+Project demonstrates ability to add, alter and delete from a SQL database with JavaScript and uses a NodeJS server.
+
+
+![Screenshot](screenshot.JPG)
+
+
+### Walkthrough Video
+
+![Walkthrough Video](video/walkthrough.mp4)
+
+
+
+
+
+
--- itemconstructor.js
+++ itemconstructor.js
@@ -25,27 +25,6 @@ Item.prototype.printItem = function() {
 };
 
 
-//			item_idnsole.log(res[i].item_id + "|	" + res[i].product_name + "|" + res[i].department_name + "|$" + res[i].price);
-//			//console.log(productArray);
-//		
-
-
-//variable that creates example of the first basic type card
-//var basic1 = new Basic (
-//"Who was the first president of the United States:", "George Washington");
-//;
-
-//console logging for debugging 
-//console.log(basic1.front);
-//console.log(basic1.back);
-
-
-///variable that creates a new basic flash card
-//var basicQues1 = new Basic(
-//	"who is your mama?", "John Adams");
-//
-//
-
 
 //exports Basic object
 module.exports = Item;
